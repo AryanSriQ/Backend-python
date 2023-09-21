5190042db94845c2575d02ca3d7cc6e70b818b5a
AryanSriQ <aryan.srivastava@qsstechnosoft.com>
2023-09-21 16:29:11 +0530

    translator using flan-t5-base

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from flask import request, Flask, send_file, jsonify
 from flask_cors import CORS
-from transformers import AutoProcessor, MusicgenForConditionalGeneration
+from transformers import AutoProcessor, MusicgenForConditionalGeneration, AutoTokenizer, AutoModelForSeq2SeqLM, T5Tokenizer, T5ForConditionalGeneration
 from diffusers import DiffusionPipeline
 import scipy
 import os
@@ -8,6 +8,7 @@ from dotenv import find_dotenv, load_dotenv
 import pymongo
 import random
 import gridfs
+import re
 
 load_dotenv(find_dotenv())
 MONGODB_URL = os.getenv("MONGODB_URL")
@@ -110,6 +111,31 @@ def get_image(image_id):
     except Exception as e:
         print("Error:", str(e))
         return "Internal Server Error", 500
+
+
+@app.route('/translate', methods=['POST'])
+def get_translation():
+
+    data = request.get_json()
+    prompt = data.get('prompt')
+    from_lang = data.get('fromLanguage')
+    to_lang = data.get('toLanguage')
+
+    tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
+    model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base")
+
+    input_text = f"translate from {from_lang} to {to_lang}: {prompt}"
+    input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+
+    outputs = model.generate(input_ids)
+    output = tokenizer.decode(outputs[0])
+
+    cleaned_text = re.sub(r'<[^>]+>', '', output).lstrip()
+    response_data = {
+        'output': cleaned_text
+    }
+
+    return jsonify(response_data)
 
 
 if __name__ == "__main__":
