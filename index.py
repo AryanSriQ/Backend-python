438458c3f54a5406fb6a0c88f56f3a186593d25f
AryanSriQ <aryan.srivastava@qsstechnosoft.com>
2023-10-06 15:59:44 +0530

    All music API - returns all the music generated by prompt to music

--- index.py
+++ index.py
@@ -10,6 +10,7 @@ import random
 import gridfs
 import re
 import cohere
+from bson import ObjectId
 
 load_dotenv(find_dotenv())
 MONGODB_URL = os.getenv("MONGODB_URL")
@@ -35,7 +36,6 @@ def getdata():
     data = request.get_json()
     prompt = data.get('prompt')
     time = data.get('time')
-    print("time: ", time)
 
     processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
     model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
@@ -49,8 +49,6 @@ def getdata():
     random_suffix = random.randint(1, 100000)
     wav_filename = f"{prompt}_{random_suffix}.wav"
     wav_filename = wav_filename.replace(" ", "_")
-
-    print(wav_filename)
 
     audio_values = model.generate(**inputs, max_new_tokens=time)
     sampling_rate = model.config.audio_encoder.sampling_rate
@@ -73,7 +71,61 @@ def getdata():
         return "File not found", 404
 
 
-@app.route('/prompt_to_text', methods=['POST'])
+@app.route('/all_music', methods=['GET'])
+def all_music():
+    try:
+        # Access your database and collection
+        db = client['music']
+        collection = db['fs.files']
+
+        # Initialize GridFS
+        fs = gridfs.GridFS(db)
+
+        documents = collection.find({})
+
+        output = []
+
+        for doc in documents:
+            file = fs.get(ObjectId(doc['_id']))
+            wav_filename = file.filename
+
+            saved_file = fs.get_last_version(filename=wav_filename)
+            output.insert(0, saved_file)
+
+        # Prepare a list of file URLs
+        file_urls = [str(file._id) for file in output]
+
+        return jsonify(file_urls)  # Send the list of file URLs to the frontend
+    except Exception as e:
+        return str(e), 500
+
+
+@app.route('/music', methods=['POST'])
+def get_music():
+    data = request.get_json()
+    wav_id = data.get('id')
+    print(wav_id)
+    try:
+        # Access your database and GridFS
+        db = client['music']
+        fs = gridfs.GridFS(db)
+
+        # Retrieve the file by file ID
+        file = fs.get(ObjectId(wav_id))
+        wav_filename = file.filename
+
+        saved_file = fs.get_last_version(filename=wav_filename)
+
+        # Send the file as a response with appropriate MIME type
+        if saved_file:
+            return send_file(saved_file, mimetype='audio/wav')
+        else:
+            return "File not found", 404
+    except Exception as e:
+        return str(e), 404
+
+
+@app.route('/pdf_chat', methods=['POST'])
 def post_data():
     req = request.get_json()
     prompt = req.get('prompt')
